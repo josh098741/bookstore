7e713d17d562cf552ee3bfa79e29ee953bb44c6a
josh098741 <morongemokaya@gmail.com>
2025-09-30 01:53:05 +0300

    Additional commits

--- frontend/src/pages/NoteDetailPage.jsx
+++ frontend/src/pages/NoteDetailPage.jsx
@@ -1,7 +1,53 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
+import {useNavigate,useParams} from 'react-router'
+import api from '../libs/axios'
+import toast from 'react-hot-toast'
+import {LoaderIcon,ArrowLeftIcon,Trash2Icon} from 'lucide-react'
+import {Link} from 'react-router'
 
 function NoteDetailPage(){
-  return (<div>Note Detail Page</div>)
+
+  const [note, setNote] = useState(null)
+  const [loading, setLoading] = useState(true)
+  const [saving, setSaving] = useState(false)
+
+  const navigate = useNavigate()
+  const {id} = useParams()
+
+  useEffect(() => {
+    const fetchNote = async () => {
+      try{
+        const res = await api.get(`/notes/${id}`)
+        setNote(res.data)
+      }catch(error){
+        console.log("Error in fetching note",error)
+        toast.error("Failed to fetch data")
+      }finally{
+        setLoading(false)
+      }
+    }
+    fetchNote()
+  },[id])
+
+  console.log({note})
+
+  if(loading){
+    return(
+      <div className="min-h-screen bg-base-200 flex items-center justify-center">
+        <LoaderIcon className="animate-spin size-10"/>
+      </div>
+    );
+  }
+
+  return (
+    <div className="min-h-screen">
+      <div className="container mx-auto px-4 py-8">
+        <div className="flex items-center justify-between mb-6">
+          <Link to="/" className=""
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default NoteDetailPage
