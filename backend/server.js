590ea1bb5349841bc435a6130c44fa2c7f9b3422
josh098741 <morongemokaya@gmail.com>
2025-09-29 11:36:26 +0300

    Additional commits

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+
+const app = express()
+
+app.get("/api/notes",(req,res) => {
+    res.send('You got  5 notes')
+})
+
+app.listen(5001,() => {
+    console.log('app listtening on port 5001')
+})
