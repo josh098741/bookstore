5a7be9a1c91bb623c3f631720cd9febe94cb4a2e
josh098741 <morongemokaya@gmail.com>
2025-09-29 18:28:17 +0300

    Additional commits

---
+++ backend/src/config/upstash.js
@@ -0,0 +1,18 @@
+require('dotenv').config()
+const {RateLimit, Analytics} = require('@upstash/ratelimit')
+const {Redis} = require('@upstash/redis')
+
+//Create Redis client using env vars
+const redis = new Redis({
+    url: process.env.UPSTASH_REDIS_REST_URL,
+    token: process.env.UPSTASH_REDIS_REST_TOKEN
+})
+
+//Create a ratelimiter that allows 10 request per 20 seconds
+const ratelimit = new RateLimit({
+    redis,
+    limiter: RateLimit.slidingWindow(10, "20 s"),
+    Analytics: true
+})
+
+module.exports = ratelimit
