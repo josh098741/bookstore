0dd15c90d4e6850dbd2d5733385f8994d898222a
josh098741 <morongemokaya@gmail.com>
2025-09-29 13:49:20 +0300

    Additional commit

---
+++ backend/src/models/Note.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+
+const noteSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: [true,"Title is required"],
+        trim: true
+    },
+    content: {
+        type: String,
+        required: [true,"Content is required"]
+    }
+},{timestamps: true})
+
+module.exports = mongoose.model("Note",noteSchema)
