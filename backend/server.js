090218f5916d16aba08cf6aa828f24413eec8860
josh098741 <morongemokaya@gmail.com>
2025-09-29 12:03:48 +0300

    Additional commit

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,18 @@ app.get("/api/notes",(req,res) => {
     res.status(200).send('You got  5 notes')
 })
 
+app.post("/api/notes",(req,res) => {
+    res.status(201).json({message: "Created a post successfully"})
+})
+
+app.put("/api/notes/:id", (req,res) => {
+    res.status(200).json({message: "Note updated successfully"})
+})
+
+app.delete("/api/notes/:id",(req,res) => {
+    res.status(200).json({messaage: "Note deleted successfully"})
+})
+
 app.listen(5001,() => {
     console.log('app listtening on port 5001')
 })
